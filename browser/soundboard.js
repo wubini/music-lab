6d3f688ca26124415befc0eb5885628f4792f76e
Kathy Lu <kaffy.lu@gmail.com>
2015-08-29 16:07:48 -0400

    ready to debug

--- browser/soundboard.js
+++ browser/soundboard.js
@@ -11,48 +11,49 @@ window.soundboard = window.EventEmitter()
 	// document.addEventListener('keydown')
 	// keyButtons.on('keydown', function())
 
-	soundboard.keypress = function(keyId, delay) {
+	soundboard.makeSoundObj = function(event) {
 
 		var sound = {
-						keyId: keyId,
-						delay: delay
+						keyId: event.keyCode
+						//delay: delay
 					};
-		soundArray.push(sound);
-		soundboard.playSound(sound);
+		//soundArray.push(sound);
+		soundboard.playOne(sound, true);
+		return sound;
 	};
 
 	soundboard.playOne = function(soundObj, shouldBroadcast)
 	{
-	  var keyId = soundObj.keyId;
-	  //map key to sound file
-	  var audio = getAudioForKey(keyId);
-	  audio.play();
+	  // var keyId = soundObj.keyId;
+	  // //map key to sound file
+	  // var audio = getAudioForKey(keyId);
+	  // audio.play();
+		console.log("playOne called on",soundObj);
+		var notePath = './sounds/piano/'+soundObj.keyId+'.wav';
+		var audio = new Audio(notePath);
+		audio.play();
 
 	  if (shouldBroadcast) {
+			console.log("soundboard emits play event");
 	    soundboard.emit('play', soundObj);
 	  }
 	}
+	//
+	// soundboard.playAll = function(soundArr)
+	// {
+	//   soundArr.forEach(function(sound)
+	//   {
+	//     setTimeout(function()
+	//     {
+	//       playOne(sound);
+	//     },delay);
+	//   }
+	// }
+	//
+	// function getAudioForKey(keyId)
+	// {
+	//   var keyToAudioMap = [];
+	//   return keyToAudioMap[keyId];
+	// }
 
-	soundboard.saveToSong = function(soundObj)
-	{
-	  soundArray.push(soundObj);
-	}
-
-	soundboard.playAll = function(soundArr)
-	{
-	  soundArr.forEach(function(sound)
-	  {
-	    setTimeout(function()
-	    {
-	      playOne(sound);
-	    },delay);
-	  }
-	}
-
-	function getAudioForKey(keyId)
-	{
-	  var keyToAudioMap = [];
-	  return keyToAudioMap[keyId];
-	}
-
-})
+})();
