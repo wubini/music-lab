081b9faf0b9ad31959b7f2b5533e3973cc8aefe8
Angelo <angelo@Angelos-MacBook-Pro.local>
2015-08-29 16:47:44 -0400

    piano keys css

--- browser/key-events.js
+++ browser/key-events.js
@@ -3,6 +3,8 @@ document.addEventListener('keyup', showKeyReleased, false);
 
 function keyPress(e) {
   var notePath = '/sounds/piano/'+e.keyCode+'.wav';
+  console.log(e.keyCode);
+  if (e.keyCode===49){notePath = '/sounds/horse.ogg'}
   var audio = new Audio(notePath);
   audio.play();
   showKeyPressed(e);
