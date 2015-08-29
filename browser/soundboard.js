42f6e71820e79636db6eeb6f258a7d5b1d7627c8
Kathy Lu <kaffy.lu@gmail.com>
2015-08-29 15:26:37 -0400

    merged soundboard stuf

--- browser/soundboard.js
+++ browser/soundboard.js
@@ -12,16 +12,47 @@ window.soundboard = window.EventEmitter()
 	// keyButtons.on('keydown', function())
 
 	soundboard.keypress = function(keyId, delay) {
-		
+
 		var sound = {
 						keyId: keyId,
 						delay: delay
 					};
 		soundArray.push(sound);
-		playSound(sound);
+		soundboard.playSound(sound);
 	};
 
+	soundboard.playOne = function(soundObj, shouldBroadcast)
+	{
+	  var keyId = soundObj.keyId;
+	  //map key to sound file
+	  var audio = getAudioForKey(keyId);
+	  audio.play();
 
+	  if (shouldBroadcast) {
+	    soundboard.emit('play', soundObj);
+	  }
+	}
 
+	soundboard.saveToSong = function(soundObj)
+	{
+	  soundArray.push(soundObj);
+	}
+
+	soundboard.playAll = function(soundArr)
+	{
+	  soundArr.forEach(function(sound)
+	  {
+	    setTimeout(function()
+	    {
+	      playOne(sound);
+	    },delay);
+	  }
+	}
+
+	function getAudioForKey(keyId)
+	{
+	  var keyToAudioMap = [];
+	  return keyToAudioMap[keyId];
+	}
 
 })
