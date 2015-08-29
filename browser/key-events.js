233f58a5806323855ace76375e3dc5b36137c5a9
Chandra Galbiati <chandragalbiati@Christophers-MacBook-Air.local>
2015-08-29 18:07:37 -0400

    piano and sockets working.  octaves are questionionable

--- browser/key-events.js
+++ browser/key-events.js
@@ -1,21 +1,23 @@
-document.addEventListener('keydown', keyPress, false);
+document.addEventListener('keydown', function(e){
+  soundboard.makeSoundObj(e);
+}, false);
 document.addEventListener('keyup', showKeyReleased, false);
-	var octave = 4;
-function keyPress(e) {
-  var code = e.keyCode;
-  if (code==38) octave++;
-  else if (code==40) octave--;
+	// var octave = 4;
+// function keyPress(e) {
+//   var code = e.keyCode;
+//   if (code==38) octave++;
+//   else if (code==40) octave--;
 
-  else {
-    var notePath = './sounds/piano/'+e.keyCode+'_'+octave+'.wav';
-    var audio = new Audio(notePath);
-    audio.play();
-  }
-  if (e.keyCode===49){notePath = '/sounds/horse.ogg'}
-  var audio = new Audio(notePath);
-  audio.play();
-  showKeyPressed(e);
-}
+//   else {
+//     var notePath = './sounds/piano/'+e.keyCode+'_'+octave+'.wav';
+//     var audio = new Audio(notePath);
+//     audio.play();
+//   }
+//   if (e.keyCode===49){notePath = '/sounds/horse.ogg'}
+//   var audio = new Audio(notePath);
+//   audio.play();
+//   showKeyPressed(e);
+// }
 
 function showKeyReleased(e) {
   var id = e.keyCode;
