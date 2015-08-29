83c2239bdd1b06d484622aa92feddc8fd9096629
Angelo <angelo@Angelos-MacBook-Pro.local>
2015-08-29 16:38:41 -0400

    piano keys css

--- browser/key-events.js
+++ browser/key-events.js
@@ -1,12 +1,19 @@
 document.addEventListener('keydown', keyPress, false);
-document.addEventListener('keyup', showKeyPress, false);
+document.addEventListener('keyup', showKeyReleased, false);
 
 function keyPress(e) {
   var notePath = '/sounds/piano/'+e.keyCode+'.wav';
   var audio = new Audio(notePath);
   audio.play();
+  showKeyPressed(e);
 }
 
-function showKeyPress(e) {
+function showKeyReleased(e) {
+  var id = e.keyCode;
+  $('#' + id).removeClass('active');
+}
 
+function showKeyPressed(e) {
+  var id = e.keyCode;
+  $('#' + id).addClass('active');
 }
