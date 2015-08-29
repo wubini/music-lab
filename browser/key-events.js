77f8854a148420b3cfc6413ec2abfb832aff4d17
Jimin Sung <jimin@Jimins-MacBook-Pro.local>
2015-08-29 16:03:17 -0400

    sounds changed

--- browser/key-events.js
+++ browser/key-events.js
@@ -2,7 +2,11 @@ document.addEventListener('keydown', keyPress, false);
 document.addEventListener('keyup', showKeyPress, false);
 
 function keyPress(e) {
-  var notePath = './sounds/piano/'+e.keyCode+'.wav';
+  var notePath = '/sounds/piano/'+e.keyCode+'.wav';
   var audio = new Audio(notePath);
   audio.play();
+}
+
+function showKeyPress(e) {
+
 }
